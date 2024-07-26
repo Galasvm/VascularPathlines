eafa6d901c10e24a0e0d2d54a1accefe126b92df
Galasvm <galasvm@berkeley.edu>
2024-07-26 16:46:17 -0700

    fixed

--- Eikonal_3D.py
+++ Eikonal_3D.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dolfinx import  fem, default_scalar_type
+from dolfinx import fem, default_scalar_type
 from dolfinx.fem import functionspace
 from dolfinx.nls.petsc import NewtonSolver
 from mpi4py import MPI
@@ -9,7 +9,7 @@ import yaml
 import os
 
 # Call the yaml file you want to use
-yaml_file = "cylinder.yaml"
+yaml_file = "demomesh.yaml"
 
 # Determine parent folder
 parent = os.path.dirname(__file__)
@@ -19,46 +19,49 @@ with open(parent + "/yaml_files/" + yaml_file, "rb") as f:
     params = yaml.safe_load(f)
 
 yaml_file_name = params["file_name"]
-save_dir = parent + "/results/" + yaml_file_name+ yaml_file_name
+save_dir = parent + "/results/" + yaml_file_name + yaml_file_name
 mesh_dir = parent + "/Meshes" + yaml_file_name + yaml_file_name
 
 
-def solve_eikonal(domain, ft, distance: bool, c = 1 ,*face_tag: int):
-
-   
+def solve_eikonal(domain, ft, distance: bool, c=1, *face_tag: int):
     V = functionspace(domain, ("Lagrange", 1))
 
-    # putting all the dofs of the faces we want to select for the boundary condition
+    # putting all the dofs of the faces we want to select for the boundary
+    # condition
     face_tags = list(face_tag)
     count_face_tags = len(face_tags)
 
     # seeing if there is more than one face selected
     if count_face_tags > 1:
-        if distance == False:
-            print("error: Should only select one face when solving solving with a seedpoint")
+        if distance is False:
+            print("error: Should only select one face" +
+                  "when solving solving with a seedpoint")
             exit()
-
 
     all = np.array([])
     for tag in face_tags:
         face = ft.find(tag)
-        all = np.append(all,face)
+        all = np.append(all, face)
 
-
-    if distance == True:
+    if distance is True:
         # defining the speed as 1 since we are looking for the distance field
         f = fem.Constant(domain, default_scalar_type(1))
-        # setting the wall of the vessel as the boundary condition 
-        domain.topology.create_connectivity(domain.topology.dim - 1, domain.topology.dim)
-        wall_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1, all)
+        # setting the wall of the vessel as the boundary condition
+        domain.topology.create_connectivity(domain.topology.dim - 1,
+                                            domain.topology.dim)
+        wall_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1,
+                                                all)
         bc = fem.dirichletbc(default_scalar_type(0), wall_dofs, V)
 
     else:
         # defining f as 1 over the distance of each node
         f = 1/(1+c)
-        domain.topology.create_connectivity(domain.topology.dim - 1, domain.topology.dim)
-        inlet_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1, all)
-        # now we need to find the dof at the center of the facet to select as the source point (the point with the greater distance from the walls)
+        domain.topology.create_connectivity(domain.topology.dim - 1,
+                                            domain.topology.dim)
+        inlet_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1,
+                                                 all)
+        # now we need to find the dof at the center of the facet to select as
+        # the source point (the point with the greater distance from the walls)
 
         c_values = c.vector.array  # Get the solution values as a numpy array
         # Find the index of the maximum value in the selected values
@@ -76,7 +79,7 @@ def solve_eikonal(domain, ft, distance: bool, c = 1 ,*face_tag: int):
         bc = fem.dirichletbc(default_scalar_type(0), inlet_dof, V)
 
 
-    # Define the unknown function and test function
+# Define the unknown function and test function
     u = fem.Function(V)
     v = ufl.TestFunction(V)
     uh = ufl.TrialFunction(V)
@@ -88,23 +91,27 @@ def solve_eikonal(domain, ft, distance: bool, c = 1 ,*face_tag: int):
     # Initialization problem to get good initial guess
     a = ufl.dot(ufl.grad(uh), ufl.grad(v)) * ufl.dx
     L = f*v*ufl.dx
-    problem = fem.petsc.LinearProblem(a, L, bcs = [bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+    problem = fem.petsc.LinearProblem(a, L, bcs=[bc],
+                                      petsc_options={"ksp_type": "preonly",
+                                                     "pc_type": "lu"})
     u = problem.solve()
 
-    #NEED TO MAKE THE EPS NUMBER RELATED TO THE MESH SIZE
+# NEED TO MAKE THE EPS NUMBER RELATED TO THE MESH SIZE
     eps = 0.1
-    F = ufl.sqrt(ufl.inner(ufl.grad(u), ufl.grad(u)))*v*ufl.dx - f*v*ufl.dx + eps * ufl.inner(ufl.grad(abs(u)), ufl.grad(v)) * ufl.dx
+    F = ufl.sqrt(ufl.inner(ufl.grad(u), ufl.grad(u)))*v*ufl.dx
+    - f*v*ufl.dx + eps * ufl.inner(ufl.grad(abs(u)), ufl.grad(v)) * ufl.dx
 
     # Jacobian of F
     J = ufl.derivative(F, u, ufl.TrialFunction(V))
 
     # Create nonlinear problem and solver
-    problem = fem.petsc.NonlinearProblem(F, u, bcs = [bc], J=J)
+    problem = fem.petsc.NonlinearProblem(F, u, bcs=[bc], J=J)
     solver = NewtonSolver(MPI.COMM_WORLD, problem)
 
     # Set solver options
     solver.rtol = 1e-6
-    solver.max_it = 80 # Increase the max iterations
+    solver.max_it = 80
+# Increase the max iterations
 
     # Solve the problem
     solver.solve(u)
@@ -116,13 +123,11 @@ def import_mesh(path_mesh, path_facets):
 
     with XDMFFile(MPI.COMM_WORLD, path_mesh, "r") as xdmf:
         domain_ = xdmf.read_mesh(name="Grid")
-        #ct = xdmf.read_meshtags(domain_, name="Grid")
-
-    domain_.topology.create_connectivity(domain_.topology.dim, domain_.topology.dim - 1)
+    domain_.topology.create_connectivity(domain_.topology.dim,
+                                         domain_.topology.dim - 1)
 
     with XDMFFile(MPI.COMM_WORLD, path_facets, "r") as xdmf:
         facet_ = xdmf.read_meshtags(domain_, name="Grid")
-    
     return domain_, facet_
 
 
@@ -134,18 +139,17 @@ def export_soln(path_export, mesh_, function_):
         file.write_function(function_)
 
 
-domain, facet = import_mesh(mesh_dir + ".xdmf", mesh_dir + "_facet_markers.xdmf")
+domain, facet = import_mesh(mesh_dir + ".xdmf",
+                            mesh_dir + "_facet_markers.xdmf")
 
-if params["multiple_wall_tag"] == True:
-    face_tags = list(range(params["wall_face_tag"],params["wall_face_tag_2"]))
-    dis = solve_eikonal(domain, facet, True, 1 , *face_tags)
+if params["multiple_wall_tag"] is True:
+    face_tags = list(range(params["wall_face_tag"], params["wall_face_tag_2"]))
+    dis = solve_eikonal(domain, facet, True, 1, *face_tags)
 
 else:
-    dis = solve_eikonal(domain, facet, True, 1 , params["wall_face_tag"])
+    dis = solve_eikonal(domain, facet, True, 1, params["wall_face_tag"])
 
+solution = solve_eikonal(domain, facet, False, dis, params["inlet_face_tag"])
 
-
-solution = solve_eikonal(domain, facet, False, dis , params["inlet_face_tag"])
-
-export_soln(save_dir + "_distance_field.xdmf",domain,dis)
-export_soln(save_dir + "_final_soln.xdmf",domain,solution)
+export_soln(save_dir + "_distance_field.xdmf", domain, dis)
+export_soln(save_dir + "_final_soln.xdmf", domain, solution)
