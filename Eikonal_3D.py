cccf99ac201c9fd9102e788c3305ecd973ba4200
Galasvm <galasvm@berkeley.edu>
2024-08-20 14:49:32 -0700

    scaling distance field

--- Eikonal_3D.py
+++ Eikonal_3D.py
@@ -10,7 +10,7 @@ import yaml
 import os
 
 # Call the yaml file you want to use
-yaml_file = "cylinder.yaml"
+yaml_file = "demomesh.yaml"
 
 # Determine parent folder
 parent = os.path.dirname(__file__)
@@ -27,7 +27,8 @@ mesh_dir = parent + "/Meshes" + yaml_file_name + yaml_file_name
 def solve_eikonal(domain, ft, distance: bool, c=1,
                   outlet_face_tag=0, *face_tag: int):
     V = functionspace(domain, ("Lagrange", 1))
-
+    domain.topology.create_connectivity(domain.topology.dim - 1,
+                                        domain.topology.dim)
     # putting all the dofs of the faces we want to select for the boundary
     # condition
     face_tags = list(face_tag)
@@ -49,8 +50,7 @@ def solve_eikonal(domain, ft, distance: bool, c=1,
     if distance is True:
 
         # setting the wall of the vessel as the boundary condition
-        domain.topology.create_connectivity(domain.topology.dim - 1,
-                                            domain.topology.dim)
+
         wall_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1,
                                                 all)
         bc = fem.dirichletbc(default_scalar_type(0), wall_dofs, V)
@@ -60,25 +60,13 @@ def solve_eikonal(domain, ft, distance: bool, c=1,
 
     else:
 
-        domain.topology.create_connectivity(domain.topology.dim - 1,
-                                            domain.topology.dim)
         inlet_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1,
                                                  all)
 
         # now we need to find the dof at the center of the facet to select as
         # the source point (the point with the greater distance from the walls)
 
-        c_values = c.vector.array  # Get the solution values as a numpy array
-        # Find the index of the maximum value in the selected values
-        selected_values = c_values[inlet_dofs]
-        max_selected_index = np.argmax(selected_values)
-
-        # Find the corresponding dof (global index)
-        max_dof = inlet_dofs[max_selected_index]
-
-        # make it into an array
-        inlet_dof = np.array([max_dof])
-
+        inlet_dof = find_ps(c, inlet_dofs)
         # define the dirichlet boundary condition at one point
         bc = fem.dirichletbc(default_scalar_type(0), inlet_dof, V)
 
@@ -94,36 +82,71 @@ def solve_eikonal(domain, ft, distance: bool, c=1,
         # then we find the node with the highest minimum distance from the
         # walls (the center) for the outlet. We also print out the highest
         # minimum distance from the walls for the inlet
+        c_values = c.vector.array
         selected_values_out = c_values[outlet_dofs]
+        selected_values_in = c_values[inlet_dofs]
         max_distance_outlet = selected_values_out.max()
-        max_distance_inlet = selected_values.max()
+        max_distance_inlet = selected_values_in.max()
         print(f"max_distance_outlet is: {max_distance_outlet}")
         print(f"max_distance_inlet is: {max_distance_inlet}")
 
         # alpha is related to the smallest vessel diameter
 
-        alpha = 20/max_distance_outlet
+        alpha = 5/max_distance_outlet
         print(f"alpha: {alpha}")
 
         # now we set the wave speed proportional to the minimum distance field
         f = 1/(2**(alpha*c))
 
     # CALCULATING EPS: We need to make eps related to the mesh size
+    # finding the max cell size now
+    hmax = h_max(domain)
+    print(f"max cell size: {hmax}")
+    eps = hmax/2
+    print(f"eps: {eps}")
+
+    u = set_problem(V, bc, eps, f)
+
+
+    if distance is True:
+        # here we want to reescale the distance field
+        dis_values = u.x.array
+        min_val = dis_values.min()
+        max_val = dis_values.max()
+        new_min = 0.01
+        new_max = 1.0
+        rescaled_values = new_min + (dis_values - min_val) * (new_max - new_min) / (max_val - min_val)
+        u = fem.Function(V)
+        u.x.array[:] = rescaled_values
+
+    return u
+
+
+def find_ps(distance_field, tag_dofs):
+    # thid function finds point source for selected face tag
+    values = distance_field.vector.array
+    inlet_values = values[tag_dofs]
+    max_inlet_index = np.argmax(inlet_values)
+    inlet_dof = np.array([tag_dofs[max_inlet_index]])
+
+    return inlet_dof
+
+
+def h_max(domain):
     num_cells = domain.topology.index_map(domain.topology.dim).size_local
     cells = np.arange(num_cells, dtype=np.int32)
     domain = dolfinx.cpp.mesh.Mesh_float64(domain.comm, domain.topology,
                                            domain.geometry)
     h = dolfinx.cpp.mesh.h(domain, domain.topology.dim, cells)
     # finding the max cell size now
-    hmax = max(h)
-    print(f"max cell size: {hmax}")
-    eps = hmax/2
-    print(f"eps: {eps}")
+    h_max = max(h)
+    return h_max
 
-    # set up the problem
-    u = fem.Function(V)
-    v = ufl.TestFunction(V)
-    uh = ufl.TrialFunction(V)
+
+def set_problem(funcspace, boundary, epsilon, f):
+    u = fem.Function(funcspace)
+    v = ufl.TestFunction(funcspace)
+    uh = ufl.TrialFunction(funcspace)
 
     # Initialize the solution to avoid convergence issues
     with u.vector.localForm() as loc:
@@ -132,18 +155,18 @@ def solve_eikonal(domain, ft, distance: bool, c=1,
     # Initialization problem to get good initial guess
     a = ufl.dot(ufl.grad(uh), ufl.grad(v)) * ufl.dx
     L = f*v*ufl.dx
-    problem = fem.petsc.LinearProblem(a, L, bcs=[bc],
+    problem = fem.petsc.LinearProblem(a, L, bcs=[boundary],
                                       petsc_options={"ksp_type": "preonly",
                                                      "pc_type": "lu"})
     u = problem.solve()
 
     F = ufl.sqrt(ufl.inner(ufl.grad(u), ufl.grad(u)))*v*ufl.dx - f*v*ufl.dx
-    F += eps * ufl.inner(ufl.grad(abs(u)), ufl.grad(v)) * ufl.dx
+    F += epsilon * ufl.inner(ufl.grad(abs(u)), ufl.grad(v)) * ufl.dx
     # Jacobian of F
-    J = ufl.derivative(F, u, ufl.TrialFunction(V))
+    J = ufl.derivative(F, u, ufl.TrialFunction(funcspace))
 
     # Create nonlinear problem and solver
-    problem = fem.petsc.NonlinearProblem(F, u, bcs=[bc], J=J)
+    problem = fem.petsc.NonlinearProblem(F, u, bcs=[boundary], J=J)
     solver = NewtonSolver(MPI.COMM_WORLD, problem)
 
     # Set solver options
@@ -153,7 +176,6 @@ def solve_eikonal(domain, ft, distance: bool, c=1,
     log.set_log_level(log.LogLevel.INFO)
 
     solver.solve(u)
-
     return u
 
 
